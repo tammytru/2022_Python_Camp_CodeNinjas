461854dbd957e90ded9224e4aa89be1cf3b4cec6
Tammy Truong <69982368+tammytru@users.noreply.github.com>
2022-05-21 12:58:15 -0500

    update pong files

--- Pong/pong.py
+++ Pong/pong.py
@@ -2,15 +2,12 @@ import pygame
 from pygame import *
 pygame.init()
 
-#===== WINDOWS AND BACKGROUND =====#
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 600
 GAME_WINDOW = display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 display.set_caption('CodeNinjas Pong')
 
-background_img = image.load('pong_background.png')
-background_surf = Surface.convert_alpha(background_img)
-BACKGROUND = transform.scale(background_surf, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
 
 game_running = True
 while game_running:
