32d1757b528f2211b94efd3bff4dba6218d4fef8
Tammy Truong <69982368+tammytru@users.noreply.github.com>
2022-06-20 19:25:24 -0700

    completed day 4 flappy source code

--- FlappyBird/flappybird.py
+++ FlappyBird/flappybird.py
@@ -1,6 +1,7 @@
 from tkinter import SCROLL
 import pygame
 from pygame import *
+import random
 pygame.init()
 
 # ========== COLORS ========== #
@@ -22,6 +23,10 @@ GROUND_SCROLL = 0
 SCROLL_SPEED = 4
 FLYING = False
 GAME_OVER = False
+
+PIPE_GAP = 150
+PIPE_FREQUENCY = 1500 #milliseconds
+LAST_PIPE = pygame.time.get_ticks() - PIPE_FREQUENCY
 
 # ========== BIRD =========== #
 class Bird(pygame.sprite.Sprite):
@@ -55,6 +60,26 @@ bird_group = pygame.sprite.Group()
 bird = Bird(100, (WINDOW_HEIGHT/2))
 bird_group.add(bird)
 
+# ========== PIPES =========== #
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, x, y, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('FlappyBird/assets/pipe.png')
+        self.rect = self.image.get_rect()
+
+        if position == 1: #at top
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(PIPE_GAP/2)]
+        if position == -1: #at bottom
+            self.rect.topleft = [x, y + int(PIPE_GAP/2)]
+
+    def update(self):
+        self.rect.x -= SCROLL_SPEED
+        if self.rect.right < 0:
+            self.kill()
+
+pipe_group = pygame.sprite.Group()
+
 # ======= LOAD IMAGES ========= #
 background_img = pygame.image.load('FlappyBird/assets/bg.png')
 ground_img = pygame.image.load('FlappyBird/assets/ground.png')
@@ -74,14 +99,34 @@ while game_running:
             if event.key == K_ESCAPE:
                 game_running = False
     
+    #if bird touch pipe
+    if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or bird.rect.top < 0:
+        GAME_OVER = True
+    
+    #if bird hit ground
+    if bird.rect.bottom >= 768:
+        GAME_OVER = True
+        FLYING = False
+
     # -------- UPDATE -------- #
     pygame.display.update()
-    if GAME_OVER == False:
+    if GAME_OVER == False and FLYING:
         GROUND_SCROLL -= SCROLL_SPEED
         if abs(GROUND_SCROLL) > 35:
             GROUND_SCROLL = 0
 
-    bird_group.update()
+        curr_time = pygame.time.get_ticks()
+        if curr_time - LAST_PIPE > PIPE_FREQUENCY:
+            pipe_height = random.randint(-100, 100)
+            btm_pipe = Pipe(WINDOW_WIDTH, int(WINDOW_HEIGHT/2) + pipe_height, -1)
+            top_pipe = Pipe(WINDOW_WIDTH, int(WINDOW_HEIGHT/2) + pipe_height, 1)
+            pipe_group.add(btm_pipe)
+            pipe_group.add(top_pipe)
+            LAST_PIPE = curr_time
+
+        pipe_group.update()
+        bird_group.update()
+
     if bird.rect.bottom > 768:
         GAME_OVER = True
         FLYING = False
@@ -90,6 +135,7 @@ while game_running:
     GAME_WINDOW.blit(ground_img, (GROUND_SCROLL, 768))
 
     bird_group.draw(GAME_WINDOW)
+    pipe_group.draw(GAME_WINDOW)
 
     pygame.display.flip()
 pygame.quit()
